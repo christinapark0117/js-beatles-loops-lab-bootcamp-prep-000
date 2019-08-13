db14dc2ae0b1edda048d2556b8f98771f19cd1b8
Christina Park <christinapark0117@gmail.com>
2019-08-13 02:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var music = [] 
+  for (let i = 0; i < musicians.length; i++) {
+    music.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return music
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var john = []
+  while (i < facts.length) {
+    john.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return john
+}
+
+function iLoveTheBeatles(number){
+  var love = []
+  var i = 0
+  i = i + 1
+  do { 
+  love.push("I love the Beatles!");
+  } while (i < 15)
+  return love
+}
